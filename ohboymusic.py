3e3b4ab69366d44a6f74790864a7e05e4ededf4b
Nur Rohman Fajri <nurrohmanfajri90@gmail.com>
2018-12-28 09:00:09 +0700

    Change sound to angklung

--- ohboymusic.py
+++ ohboymusic.py
@@ -35,10 +35,10 @@ class PianoStairs():
       self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
       
     letters = [
-      "c0", "d1", "e1", "f1", "g1", "a1", "b1",
-      "c1", "d2", "e2", "f2", "g2", "a2", "b2",
-      "c2", "d3", "e3", "f3", "g3", "a3", "b3",
-      "c3", "d4", "e4", "f4", "g4", "a4", "b4",
+      "00-g3", "01-a3", "02-b3", "03-c4", "04-d4", "05-e4", "06-f-4",
+      "07-g4", "08-a3", "09-b3", "10-c4", "11-d4", "12-e4", "13-f-4",
+      "14-g3", "15-a3", "16-b3", "17-c4", "18-d4", "19-e4", "20-f-4",
+      "21-g4", "22-a3", "23-b3", "24-c4", "25-d4", "26-e4"
     ]
 
     # remove this line if you plugged the sensors in top-to-bottom ;)
@@ -52,7 +52,8 @@ class PianoStairs():
     self.channels = [pygame.mixer.Channel(i) for i in range(7)]
 
   def piano(self, i):
-    self.channels[i%7].play(self.sounds[i])
+    if i <= 26:
+      self.channels[i%7].play(self.sounds[i])
 
   def run(self):
     # Sleep while we wait for everything to boot up.
